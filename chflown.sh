e3982df95cd355f1047e78ea38157ff1e70c1690
ivan-stanislav <ivan.stanislav.ua@gmail.com>
2021-12-20 05:18:26 +0200

    The final coomit

--- chflown.sh
+++ chflown.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
-
 curusrid=`id | grep uid=0`
-user=$1
-directory=$2
-
-if [ -n "$curusrid" ];
+if [ -z "`id | grep uid=0`" ];
   then 
-   echo -e  "\e[32mYou are the root and allowed to run $0 script\e[0m"
+      echo -e "\e[31mYou are not the root and have no permission to run $0 script\e[0m"
+      exit
   else
-   echo -e "\e[31mYou are not the root and have not permission to run $0 script\e[0m"
-   exit
-fi
-
-if [ -z "$1" ];
- then  echo -e "\e[31mUser must be set\e[0m"
- exit
-  elif [ -z "$2" ];
-   then  echo -e "\e[31mDirectory must be set\e[0m"
-   exit
-else
-
-
-
- usr=`cat /etc/passwd | grep "$1"`
- echo $usr
-# if [ -n "$usr" ];
-#     then echo -e "\e[32mUser $user have been found\e[0m"
-#        if [ -n "$dir" ];
-#          then dir=`ls-l | grep'
-#          else echo -e "\e[31mDirectory must be set\e[0m
-#        fi
-#     else echo -e "\e[31mDirectory not found\e[0m"
-#     else echo -e "\e[31mUser $user not found\e[0m"
-#    exit
-
-sudo chown -R $1 $2
-
+      echo -e  "\e[32mYou are the root and allowed to run $0 script\e[0m"
+      if [ -z "$1" ];
+        then
+            echo -e "\e[31mUser must be set\e[0m"
+            exit
+         elif [ -z "$2" ];
+             then
+                 echo -e "\e[31mDirectory must be set\e[0m"
+                 exit
+         else
+             if [ -n "`cat /etc/passwd | grep -w $1`" ]; 
+               then
+                   echo -e  "\e[32mUser $1 has been found\e[0m"
+                   dir=`ls -l | grep -w $2`
+                   if [ -n "$dir" ];
+                     then
+                        echo -e "\e[32mDirectory $2 has been found\e[0m"
+                        sudo chown -R $1 $2
+                        echo -e "\e[32m$2 directory owner sucsesfully changed to $1\e[0m"
+                     else
+                        echo -e "\e[31mDirectory not found\e[0m"
+                        exit
+                   fi
+               else
+                 echo -e "\e[31mUser have $1 been not found\e[0m"
+             fi
+      fi
 fi
